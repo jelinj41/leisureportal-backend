f1087452bb9334784eec736648bfa03ddcf479ff
Jaroslav Jelínek <jelinj41@fel.cvut.cz>
2023-05-11 23:25:06 +0200

    Adding backend v.21 to github.

--- src/main/java/cz/cvut/fel/bp/leisureportalbackend/config/SecurityConfig.java
+++ src/main/java/cz/cvut/fel/bp/leisureportalbackend/config/SecurityConfig.java
@@ -18,6 +18,11 @@ import org.springframework.security.web.authentication.AuthenticationFailureHand
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 
 @Configuration
@@ -61,20 +66,33 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*")); // allow all origins
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS")); // allow only GET and POST requests
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token")); // allow these headers
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token")); // expose the X-Auth-Token header
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().authorizeRequests().anyRequest().permitAll().and()
-            .exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-            .and().headers().frameOptions().sameOrigin()
-            .and().authenticationProvider(authenticationProvider)
-            .formLogin().successHandler(authenticationSuccessHandler)
-            .failureHandler(authenticationFailureHandler)
-            .loginProcessingUrl(SecurityConstants.SECURITY_CHECK_URI)
-            .usernameParameter(SecurityConstants.USERNAME_PARAM).passwordParameter(SecurityConstants.PASSWORD_PARAM)
-            .and()
-            .logout().invalidateHttpSession(true).deleteCookies(COOKIES_TO_DESTROY)
-            .logoutUrl(SecurityConstants.LOGOUT_URI).logoutSuccessHandler(logoutSuccessHandler)
-            .and().sessionManagement().maximumSessions(1);
+        http.cors().configurationSource(corsConfigurationSource())
+                .and().csrf().disable().authorizeRequests().anyRequest().permitAll().and()
+                .exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                .and().headers().frameOptions().sameOrigin()
+                .and().authenticationProvider(authenticationProvider)
+                .formLogin().successHandler(authenticationSuccessHandler)
+                .failureHandler(authenticationFailureHandler)
+                .loginProcessingUrl(SecurityConstants.SECURITY_CHECK_URI)
+                .usernameParameter(SecurityConstants.USERNAME_PARAM).passwordParameter(SecurityConstants.PASSWORD_PARAM)
+                .and()
+                .logout().invalidateHttpSession(true).deleteCookies(COOKIES_TO_DESTROY)
+                .logoutUrl(SecurityConstants.LOGOUT_URI).logoutSuccessHandler(logoutSuccessHandler)
+                .and().sessionManagement().maximumSessions(1);
     }
 
     @Override
