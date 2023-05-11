50aab20cd3010d2c12156c5aad895c37802461c8
Jaroslav Jelínek <jelinj41@fel.cvut.cz>
2023-05-11 23:54:35 +0200

    Adding backend v.25 to github.

--- src/main/java/cz/cvut/fel/bp/leisureportalbackend/config/WebMvcConfig.java
+++ src/main/java/cz/cvut/fel/bp/leisureportalbackend/config/WebMvcConfig.java
@@ -1,5 +1,9 @@
 package cz.cvut.fel.bp.leisureportalbackend.config;
 
+import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
+import org.apache.tomcat.util.http.SameSiteCookies;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -9,6 +13,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final long MAX_AGE_SECS = 3600;
+
+    @Bean
+    public TomcatContextCustomizer sameSiteCookiesConfig() {
+        return context -> {
+            final Rfc6265CookieProcessor cookieProcessor = new Rfc6265CookieProcessor();
+            cookieProcessor.setSameSiteCookies(SameSiteCookies.NONE.getValue());
+            context.setCookieProcessor(cookieProcessor);
+        };
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
