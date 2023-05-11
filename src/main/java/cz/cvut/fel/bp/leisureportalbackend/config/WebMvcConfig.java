90c1783b0dd5d2adee347bfbffcccf461bbddd1f
Jaroslav Jelínek <jelinj41@fel.cvut.cz>
2023-05-11 21:25:50 +0200

    Adding backend v.17 to github.

--- src/main/java/cz/cvut/fel/bp/leisureportalbackend/config/WebMvcConfig.java
+++ src/main/java/cz/cvut/fel/bp/leisureportalbackend/config/WebMvcConfig.java
@@ -12,10 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000").allowCredentials(true)
-                .allowedOrigins("https://leisureportal-frontend.herokuapp.com").allowCredentials(true)
-                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+        registry.addMapping("/**").allowedMethods("*")
                 .maxAge(MAX_AGE_SECS);
     }
 
